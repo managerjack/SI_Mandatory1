27042ac7cb2f109245c275df7463dce24c17b5f6
Arvid Larsen <Arvidanker95@gmail.com>
2020-09-30 09:01:58 +0200

    Added node password generator

---
+++ NemID_PasswordGenerator/password_generator.js
@@ -0,0 +1,29 @@
+const port = 8089;
+const express = require('express');
+
+var app = express();
+
+app.use(express.json());
+
+
+app.post('/generate-password-nemID', (req, res) =>{
+    let nemId = req.body.nemId;
+    let cpr = req.body.cpr;
+    // First 2 digits of nemid
+    let firstNemId = cpr.slice(0, 2);
+    // Last 2 digits of cpr
+    let lastCpr = cpr.slice(cpr.length - 2);
+    let password = firstNemId.toString() + lastCpr.toString();
+    console.log(password);
+    res.status(201).send({nemIdPassword: password});
+});
+
+app.listen(port, (err) => {
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("Listening on port " + port);
+        console.log("Password generator is running...");
+    }
+});
--- NemID_UserGenerator/user_generator.js
+++ NemID_UserGenerator/user_generator.js
@@ -1,4 +1,4 @@
-const port = 8088
+const port = 8088;
 const express = require('express');
 
 var app = express();
