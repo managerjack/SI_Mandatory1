4f9912bd44d218d6d04d134b676cf9dea3c5965d
Arvid Larsen <Arvidanker95@gmail.com>
2020-10-02 11:37:30 +0200

    Fixed proof images

--- NemID_CodeGenerator/auth_code_generator.js
+++ NemID_CodeGenerator/auth_code_generator.js
@@ -13,10 +13,10 @@ var db = new sqlite3.Database('../NemID_ESB/nem_id_database.sqlite');
 app.use(express.json());
 
 
-app.post('/nemid_auth', (req, res) =>{
+app.post('/nemid-auth', (req, res) =>{
     let nemId = req.body.nemId.toString();
     let password = req.body.nemIdCode.toString();
-
+    
     let queryFindUser = "SELECT * FROM user WHERE NemID = ? AND Password = ?"
 
     // Retreive user from database
@@ -25,15 +25,14 @@ app.post('/nemid_auth', (req, res) =>{
             rand_code = "";
             for(i = 0; i < 6; i++)
                 rand_code += Math.floor(Math.random() * Math.floor(10)).toString();
-
-            res.status(200).send({nemIdPassword: rand_code});
+            
+            res.status(200).send({generatedCode: rand_code});
         }else 
             res.sendStatus(403);
         if(err){
             console.log(err);
         }
     });
-    //res.status(status).send(msg);
 });
 
 app.listen(port, (err) => {
